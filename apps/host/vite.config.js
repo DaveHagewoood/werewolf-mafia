37725c058fc52e38b7b32f55b474b0e58ab1f225
DaveHagewood <davezilla85@gmail.com>
2025-06-18 15:00:22 +0300

    Config changes to try to get deployment working

--- apps/host/vite.config.js
+++ apps/host/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +12,10 @@ export default defineConfig({
       '127.0.0.1',
       'werewolf-host.serveo.net' // Allow Serveo host domain
     ]
+  },
+  resolve: {
+    alias: {
+      '@werewolf-mafia/shared': path.resolve(__dirname, '../../packages/shared/index.js')
+    }
   }
 }) 
